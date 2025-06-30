0f621ba5adfe9da13e0c43e7392e14ffc166a54e
tejasdulewale <157590781+tejasdulewale@users.noreply.github.com>
2025-06-30 13:12:39 +0530

    Update MyProject.jsx

--- client/src/components/MyProject.jsx
+++ client/src/components/MyProject.jsx
@@ -6,12 +6,10 @@ const MyProject = () => {
     <div className="myproject" id="myproject">
       <div className="workname">My Work</div>
       <h2 className="recentproject">RECENT PROJECT</h2>
-
       <div className="projectcardcontainer">
            <ProjectSlide/>
       </div>
     </div>
   );
 };
-
 export default MyProject;
